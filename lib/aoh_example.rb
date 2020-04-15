5098c8414a88102c8c1e99184aaec5c3625b423e
Guosa Edebiri <gedebiri@gmail.com>
2020-04-15 08:39:14 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -23,7 +23,7 @@ def literal_aoh
     { :name => "Joelle van Dyne", :occupation => "Radio Personality"}
     { :name => "Pat Monteseian", :occupation => "Staff"}
     { :name => "Kate Gompert", :occupation => "None"}
-    { :name => "Don Gately", :occupation => "Live-in Staff"}
+    { :name => "Bruce Green", :occupation => "Live-in Staff"}
   ]
   
 end
